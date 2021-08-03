bc8dfbc7796057a73e0f78afaddb685798d45986
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-08-02 23:32:22 -0700

    adding falling hearts

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+const HEARTS = {
+  quantity: 40,
+  speed: {
+    min: 3,
+    max: 5
+  },
+  delay: {
+    min: 0,
+    max: 5
+  },
+  size: {
+    min: 2,
+    max: 7
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log("bouba")
+
+for (let i = 0; i < HEARTS.quantity; i++) {
+  const heart = document.createElement("img")
+  heart.className = "bouba heart clockwise"
+  if (Math.round(Math.random())) {
+    heart.className = "bouba heart counterclockwise"
+  }
+  heart.src = "assets/images/bouba-heart.png"
+  heart.style.animationDuration = (((HEARTS.speed.min * 100) + Math.floor(Math.random() * ((HEARTS.speed.max - HEARTS.speed.min) * 100))) / 100).toString() + "s"
+  heart.style.animationDelay = (((HEARTS.delay.min * 100) + Math.floor(Math.random() * ((HEARTS.delay.max - HEARTS.delay.min) * 100))) / 100).toString() + "s"
+  heart.style.left = (Math.round(Math.random() * 1000) / 10).toString() + "vw"
+  heart.style.height =  (((HEARTS.size.min * 100) + Math.floor(Math.random() * ((HEARTS.size.max - HEARTS.size.min) * 100))) / 100).toString() + "vh"
+  document.body.appendChild(heart)
+}
